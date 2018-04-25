8102c92ab9e3b6f7c6d24c0123120b1efd45f346
codeepic <ksaweryglab@gmail.com>
2018-04-26 00:37:39 +0100

    feat: fetch users from json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    users: IUser[];
+
+    constructor(private http: HttpClient){
+        this.getJSON()
+            .subscribe((data: IUsers) => {
+                this.users = data.users;
+                console.error('%c users fetched ', 'background: green; color: white', this.users)
+            }, error => {
+                console.error('%c something went wrong ', 'background: red; color: white', error)
+            })
+    }
+
+    getJSON(): Observable<IUsers>{
+        return this.http.get<IUsers>('./assets/users.json');
+    }
+
+}
+
+interface IUsers {
+    users: IUser[]
+}
+
+interface IUser{
+    guid: string,
+    name: {
+        first: string,
+        last: string
+    },
+    email: string,
+    phone: string
 }
